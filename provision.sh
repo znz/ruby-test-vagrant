0a74a8523cfedfacc8d95f981d4b139975052e11
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-05-19 23:18:10 +0900

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 export DEBIAN_FRONTEND=noninteractive
+apt-get update || :
 apt-get install -y etckeeper git
 sed -i -e 's/^#VCS="git"/VCS="git"/' -e 's/^VCS="bzr"/#VCS="bzr"/' -e 's/^GIT_COMMIT_OPTIONS=""/GIT_COMMIT_OPTIONS="-v"/' /etc/etckeeper/etckeeper.conf
 etckeeper init 'Initial commit'
@@ -11,8 +12,7 @@ etckeeper commit 'Use JP mirror' || :
 apt-get update || :
 apt-get purge -y nano
 apt-get install -y vim
-#apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
-apt-get install -y autoconf bison build-essential libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
+apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
 apt-get install -y lv
 apt-get install -y clang
 apt-get install -y ccache
@@ -22,7 +22,7 @@ apt-get install -y language-pack-ja || {
   sed -i -e 's/^# ja_JP.UTF-8/ja_JP.UTF-8/' /etc/locale.gen
   locale-gen
 }
-update-locale LANG=ja_JP.UTF-8
+localectl set-locale LANG=ja_JP.UTF-8 || update-locale LANG=ja_JP.UTF-8
 etckeeper commit 'Setup Japanese locale' || :
 timedatectl set-timezone Asia/Tokyo || ln -sf ../usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 etckeeper commit 'Setup Japanese timezone' || :
