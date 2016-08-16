8453221f851f03fd15ce89a6e80a6cb64b434c30
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-08-16 20:33:31 +0900

    Use `[[ ]]` instead of `[ ]`

--- provision-anyenv.sh
+++ provision-anyenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
-if [ ! -d "$HOME/.anyenv" ]; then
+if [[ ! -d "$HOME/.anyenv" ]]; then
   git clone https://github.com/riywo/anyenv "$HOME/.anyenv"
 fi
 if ! grep -q anyenv "$HOME/.bashrc"; then
@@ -13,12 +13,12 @@ export PATH="$HOME/.anyenv/bin:$PATH"
 set +x
 eval "$(anyenv init - --no-rehash)"
 set -x
-if [ ! -d "$(anyenv root)/plugins/anyenv-update" ]; then
+if [[ ! -d "$(anyenv root)/plugins/anyenv-update" ]]; then
   git clone https://github.com/znz/anyenv-update "$(anyenv root)/plugins/anyenv-update"
 fi
-if [ ! -d "$(anyenv root)/plugins/anyenv-git" ]; then
+if [[ ! -d "$(anyenv root)/plugins/anyenv-git" ]]; then
   git clone https://github.com/znz/anyenv-git "$(anyenv root)/plugins/anyenv-git"
 fi
-if [ ! -d "$HOME/.anyenv/envs/rbenv" ]; then
+if [[ ! -d "$HOME/.anyenv/envs/rbenv" ]]; then
   anyenv install rbenv
 fi
