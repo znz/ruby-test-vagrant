03ed629e02cf2f412e04e73d690e01f848c479d0
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-08-16 20:56:27 +0900

    Install under $HOME/opt instead of $HOME/local

--- provision-libressl.sh
+++ provision-libressl.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 set -euxo pipefail
-VERSION=2.4.0
+VERSION=2.4.2
 CACHE_DIR="/vagrant/libressl"
+BUILD_DIR="$HOME/build"
 mkdir -p "$CACHE_DIR"
 cd "$CACHE_DIR"
 if ! gpg --list-keys | grep -q D5E4D8D5; then
-  curl http://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl.asc | gpg --import
+  if [[ ! -f "libressl.asc" ]]; then
+    wget -N http://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl.asc
+  fi
+  gpg --import "libressl.asc"
 fi
-if [ ! -f "libressl-${VERSION}.tar.gz" ]; then
+if [[ ! -f "libressl-${VERSION}.tar.gz" ]]; then
   wget -N "http://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-${VERSION}.tar.gz"{,.asc}
 fi
 gpg "libressl-${VERSION}.tar.gz.asc"
-cd "$HOME"
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
 if [ ! -d "libressl-${VERSION}" ]; then
   tar zxf "$CACHE_DIR/libressl-${VERSION}.tar.gz"
 fi
 cd "libressl-${VERSION}"
-./configure --prefix="$HOME/local"
+./configure --prefix="$HOME/opt/libressl-${VERSION}"
 make
 make check
 make install
