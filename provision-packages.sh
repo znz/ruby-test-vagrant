5f9c56acf1e2184a2173640fc8431212723d6448
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-08-16 23:34:50 +0900

    Install clang

--- provision-packages.sh
+++ provision-packages.sh
@@ -7,8 +7,11 @@ apt-get install -y cron-apt
 apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
 apt-get install -y byobu
 apt-get install -y ccache
+apt-get install -y clang
 apt-get install -y curl
 apt-get install -y golang
 apt-get install -y subversion
 apt-get install -y ruby
-[ "$(lsb_release -cs)" = "precise" ] && apt-get install -y ruby1.9.1
+if [ "$(lsb_release -cs)" = "precise" ]; then
+  apt-get install -y ruby1.9.1
+fi
