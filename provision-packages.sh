218c6b8d5d63d8f5f4412240b52a7f1afc44ec06
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-08-17 22:18:47 +0900

    Allow to fail to go get

--- provision-packages.sh
+++ provision-packages.sh
@@ -9,7 +9,6 @@ apt-get install -y byobu
 apt-get install -y ccache
 apt-get install -y clang
 apt-get install -y curl
-apt-get install -y golang
 apt-get install -y subversion
 apt-get install -y ruby
 if [ "$(lsb_release -cs)" = "precise" ]; then
