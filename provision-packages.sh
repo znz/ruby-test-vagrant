76583139f874fe1ffb24c993f99aeae45e3ef154
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-11-13 23:04:00 +0900

    Use unattended-upgrades instead of cron-apt

--- provision-packages.sh
+++ provision-packages.sh
@@ -3,7 +3,6 @@ set -euxo pipefail
 export DEBIAN_FRONTEND=noninteractive
 apt-get purge -y nano
 apt-get install -y vim
-apt-get install -y cron-apt
 apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
 apt-get install -y byobu
 apt-get install -y ccache
