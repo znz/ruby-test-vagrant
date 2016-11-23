036abfe9f2188fd28bbdf23972ab8b558c8853ae
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-11-23 16:27:02 +0900

    Update packages

--- provision-packages-misc.sh
+++ provision-packages-misc.sh
@@ -2,5 +2,7 @@
 set -euxo pipefail
 export DEBIAN_FRONTEND=noninteractive
 apt-get install -y lv
+apt-get install -y shellcheck
+apt-get install -y subversion
 apt-get install -y zsh
 [ "$(lsb_release -cs)" = "precise" ] || apt-get install -y silversearcher-ag
--- provision-packages.sh
+++ provision-packages.sh
@@ -8,7 +8,6 @@ apt-get install -y byobu
 apt-get install -y ccache
 apt-get install -y clang
 apt-get install -y curl
-apt-get install -y subversion
 apt-get install -y ruby
 if [ "$(lsb_release -cs)" = "precise" ]; then
   apt-get install -y ruby1.9.1
