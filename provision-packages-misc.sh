7f20471633ae793d1bfd9b85f0a223fc6c862223
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-11-23 17:01:22 +0900

    Update packages

--- provision-packages-misc.sh
+++ provision-packages-misc.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -euxo pipefail
 export DEBIAN_FRONTEND=noninteractive
+apt-get install -y aptitude
+apt-get install -y clang
 apt-get install -y lv
 apt-get install -y shellcheck
+apt-get install -y source-highlight
 apt-get install -y subversion
+apt-get install -y vim-editorconfig
 apt-get install -y zsh
 [ "$(lsb_release -cs)" = "precise" ] || apt-get install -y silversearcher-ag
--- provision-packages.sh
+++ provision-packages.sh
@@ -6,7 +6,6 @@ apt-get install -y vim
 apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
 apt-get install -y byobu
 apt-get install -y ccache
-apt-get install -y clang
 apt-get install -y curl
 apt-get install -y ruby
 if [ "$(lsb_release -cs)" = "precise" ]; then
@@ -14,3 +13,5 @@ if [ "$(lsb_release -cs)" = "precise" ]; then
 fi
 echo unattended-upgrades unattended-upgrades/enable_auto_updates boolean true | debconf-set-selections
 apt-get install -y unattended-upgrades
+sed -i -e 's,//\(Unattended-Upgrade::Remove-Unused-Dependencies "\)false\(";\),\1true\2,' /etc/apt/apt.conf.d/50unattended-upgrades
+etckeeper commit 'Enable autoremove with unattended-upgrades' || :
