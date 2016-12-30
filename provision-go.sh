bb2527b76600ccc191f3ee6ad09476704f91c46e
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-12-30 14:05:01 +0900

    comment out peco

--- provision-go.sh
+++ provision-go.sh
@@ -7,6 +7,6 @@ fi
 if [[ ! -x "$HOME/g/bin/ghq" ]]; then
   go get -x -u github.com/motemen/ghq
 fi
-if [[ ! -x "$HOME/g/bin/peco" ]]; then
-  go get -x -u github.com/peco/peco/cmd/peco
-fi
+#if [[ ! -x "$HOME/g/bin/peco" ]]; then
+#  go get -x -u github.com/peco/peco/cmd/peco
+#fi
